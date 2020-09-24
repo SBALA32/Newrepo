829742d9f8facbfae441f5563d88fdb9334604e8
SBALA32 <46257020+SBALA32@users.noreply.github.com>
2020-09-24 18:52:03 +0530

    Create Byeprogram.cs
    This is my second Bye World program on GIT

---
+++ Byeprogram.cs
@@ -0,0 +1,11 @@
+namespace Byeworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bye World!");
+            Console.ReadKey();
+        }
+    }
+}
